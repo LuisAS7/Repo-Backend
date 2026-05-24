b9daa0a6e9f385e9f9ab1e5ec315ed57599e4709
LuisAS7 <luioangelito7@gmail.com>
2026-05-23 20:04:54 -0500

    refactor: simplify declarative base configuration

--- app/models/base.py
+++ app/models/base.py
@@ -1,8 +1,4 @@
-from sqlalchemy.orm import DeclarativeBase, declared_attr
-from datetime import datetime
+from sqlalchemy.orm import DeclarativeBase
 
 class Base(DeclarativeBase):
-    # Define common columns for all tables
-    @declared_attr.directive
-    def __tablename__(cls) -> str:
-        return cls.__name__.lower()
+    pass
