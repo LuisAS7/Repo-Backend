39014daa16c43d8e4ae3d339e70ec97271146411
Frank-Vidal-Alamo <frankvidal06@gmail.com>
2026-06-04 16:49:29 -0500

    feat: implement RBAC security and restructure all API v1 routers

--- app/api/v1/routers/catalogs_router.py
+++ app/api/v1/routers/catalogs_router.py
@@ -5,13 +5,13 @@ Used by the frontend to populate dropdown menus and selection lists.
 from fastapi import APIRouter, Depends, status
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.api.deps import get_db
+from app.api.deps import get_current_user, get_db
 from app.schemas.patients_schema import AllergyResponse, ChronicDiseaseResponse
 from app.schemas.users_schema import SpecialtyResponse
 from app.schemas.appointments_schema import DiagnosisCatalogResponse
 from app.services import catalogs_service
 
-router = APIRouter(prefix="/catalogs", tags=["Catalogs & References"])
+router = APIRouter(prefix="/catalogs", tags=["Catalogs & References"],dependencies=[Depends(get_current_user)])
 
 @router.get("/allergies", response_model=list[AllergyResponse], status_code=status.HTTP_200_OK)
 async def get_allergies(session: AsyncSession = Depends(get_db)):
